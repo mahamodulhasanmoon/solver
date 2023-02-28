0e32b640ef8e543be7ac50c4518daa21f00b2880
Mahamodul Hasan Moon <coddermoon@gmail.com>
2023-03-01 02:19:44 +0600

    done form section

--- src/pages/Ask/AskForm.js
+++ src/pages/Ask/AskForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ShowCaseFooter from '../shared/ShowcaseLayout/ShowCaseFooter';
-import ShowcaseHeader from '../shared/ShowcaseLayout/ShowcaseHeader';
+
 
 const AskForm = () => {
     return (
@@ -8,26 +8,26 @@ const AskForm = () => {
         <div className="min-h-[70vh]">
            
            {/* header  */}
-           <div className="header sticky top-0 z-50 bg-white flex justify-between px-3 py-2 border-b border-b-darkShade ">
-        <h2 className="text-darkShade text-center w-full">Category Name</h2>
+           <div className="header sticky top-0 z-50 bg-white flex justify-between px-3 py-4 custom-shadow ">
+        <h2 className="text-primaryColor text-center w-full ">How To Ask</h2>
 
       </div>
       {/* form */}
-<form className='p-3'>
+<form className='p-3 '>
 
 <div>
 <label htmlFor="Category">Category</label> <br />
-<input className='w-full mt-2 border-t border-t-primaryColor focus:outline-none' type="text" name='category ' placeholder='Type your “ask” category 1 to 3 word'/>
+<input className='w-full mt-2 border-t border-top focus:outline-none' type="text" name='category ' placeholder='Type your “ask” category 1 to 3 word'/>
 </div>
 
-<div className='mt-8'>
+<div className='mt-4'>
 <label htmlFor="Title">Title</label> <br />
-<input className='w-full mt-2 border-t border-t-primaryColor focus:outline-none' type="text" name='Title ' placeholder='Type your “ask” category 1 to 3 word'/>
+<input className='w-full mt-2 border-t border-top focus:outline-none' type="text" name='Title ' placeholder='Type your “ask” category 1 to 3 word'/>
 </div>
 
-<div className='mt-8'>
+<div className='mt-4'>
 <label htmlFor="Title">Details</label> <br />
-<textarea className='w-full h-[100px] mt-2 border-t border-t-primaryColor focus:outline-none '  name='Title ' placeholder='( Optional )Type details with attachment. You can submit
+<textarea className='w-full h-[100px] mt-2 border-t border-top focus:outline-none '  name='Title ' placeholder='( Optional )Type details with attachment. You can submit
 question without this option.'/>
 </div>
 
--- src/pages/shared/ShowcaseLayout/ShowCaseFooter.js
+++ src/pages/shared/ShowcaseLayout/ShowCaseFooter.js
@@ -6,7 +6,7 @@ import { AiOutlineDown } from "react-icons/ai";
 
 const ShowCaseFooter = () => {
   return (
-    <div className="footer mt-auto flex justify-between px-3 pt-4  border-t border-t-darkShade sticky bottom-0 z-50 bg-white  w-full">
+    <div className="footer mt-auto flex justify-between px-3 pt-4  custom-shadow2 sticky bottom-0 z-50 bg-white  w-full">
       <div className="flex justify-between items-center w-full">
         <div className="flex items-center gap-10">
           <GrAttachment />
@@ -17,7 +17,7 @@ const ShowCaseFooter = () => {
             <FaUserFriends />
             <AiOutlineDown></AiOutlineDown>
           </div>
-          <button className="text-bgColor bg-primaryColor rounded border inline-block border-primaryColor px-4 py-2">
+          <button className="text-bgColor bg-primaryColor rounded border inline-block border-primaryColor px-4 py-1">
             Ask
           </button>
         </div>
